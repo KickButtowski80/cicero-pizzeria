4196fcb5cbe944fca377f484bde7d8c54cecbad9
kickbuttowski80 <pazpaz25@gmail.com>
2024-01-31 20:28:16 -0800

    animate the menu

--- src/js/toggleHamburgerMenu.js
+++ src/js/toggleHamburgerMenu.js
@@ -1,10 +1,22 @@
 const hamburgerIcon = document.querySelector('#hamburgerIcon')
-const  menuBg = document.querySelector('.menu-bg')
+const menuBg = document.querySelector('.menu-bg')
 const menuItems = document.querySelectorAll('#menu-items')
 const navBar = document.querySelector('#navbar');
 hamburgerIcon.addEventListener('click', () => {
     hamburgerIcon.classList.toggle('change');
-    menuBg.classList.toggle('in-view');
-    menuItems[0].classList.toggle('hidden');
+    toggleClasses(menuBg, 'in-view', 'out-view');
+    toggleClasses(menuItems[0], 'in-view-menuItems', 'out-view-menuItems');
 
-})
+
+});
+
+function toggleClasses(element, class1, class2) {
+    if (element.classList.contains(class1)) {
+        element.classList.remove(class1);
+        element.classList.add(class2);
+    } else {
+        element.classList.remove(class2);
+        element.classList.add(class1);
+    }
+}
+
