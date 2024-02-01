1c7dac87a46869f94a87cbd1883920db945f7181
kickbuttowski80 <pazpaz25@gmail.com>
2024-02-01 14:45:52 -0800

    play with logo animation

--- src/js/toggleHamburgerMenu.js
+++ src/js/toggleHamburgerMenu.js
@@ -1,12 +1,12 @@
 const hamburgerIcon = document.querySelector('#hamburgerIcon')
 const menuBg = document.querySelector('.menu-bg')
 const menuItems = document.querySelectorAll('#menu-items')
-const navBar = document.querySelector('#navbar');
+const navBarLogo = document.querySelector('#logo');
 hamburgerIcon.addEventListener('click', () => {
     hamburgerIcon.classList.toggle('change');
     toggleClasses(menuBg, 'in-view', 'out-view');
     toggleClasses(menuItems[0], 'in-view-menuItems', 'out-view-menuItems');
-
+    toggleClasses( navBarLogo, 'in-view-logo', 'out-view-logo');
 
 });
 
