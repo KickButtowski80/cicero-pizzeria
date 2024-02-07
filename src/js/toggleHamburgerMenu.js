9cccba232a2d296c0b50cd32ed65074975610d4e
kickbuttowski80 <pazpaz25@gmail.com>
2024-02-06 20:03:15 -0800

    rid of debugger

--- src/js/toggleHamburgerMenu.js
+++ src/js/toggleHamburgerMenu.js
@@ -1,7 +1,6 @@
 const hamburgerIcon = document.querySelector('#hamburgerIcon')
 const menuBg = document.querySelector('.menu-bg')
-const flyoutMenuId = document.querySelectorAll('#flyout-menu-items')
-// debugger;
+const flyoutMenuId = document.querySelectorAll('#flyout-menu-items');
 const flyoutMenuItems = document.querySelectorAll('#flyout-menu-items li');
 const navBarLogo = document.querySelector('#flyout-menu-logo');
 hamburgerIcon.addEventListener('click', () => {
