9f6990bd67a0fac773754bfdac30d8d996a4e8c9
kickbuttowski80 <pazpaz25@gmail.com>
2024-02-06 21:45:18 -0800

    highlight current section inside the static menu

---
+++ src/js/highlightActiveSection.js
@@ -0,0 +1,19 @@
+const allMenuItems = document.querySelectorAll('#flyout-menu-items li a, #static-menu-items li a');
+
+const options = {
+    threshold: 0.3
+}
+allMenuItems.forEach(menuItem => {
+    const hashId = menuItem.hash;
+    const observedElement = document.querySelector(hashId);
+    let observer = new IntersectionObserver((entries) => {
+        const [entry] = entries;
+        if (entry.isIntersecting) {
+            menuItem.parentElement.classList.add("active");
+        } else {
+            menuItem.parentElement.classList.remove("active");
+        }
+
+    }, options);
+    observer.observe(observedElement)
+})
