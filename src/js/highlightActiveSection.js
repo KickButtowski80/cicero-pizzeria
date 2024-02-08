991caa0141f8457ec6697dc0a984325809f7038a
kickbuttowski80 <pazpaz25@gmail.com>
2024-02-07 18:17:59 -0800

    format Fixes #16

--- src/js/highlightActiveSection.js
+++ src/js/highlightActiveSection.js
@@ -1,23 +1,20 @@
 (() => {
+    let allMenuItems = document.querySelectorAll('#flyout-menu-items li a, #static-menu-items li a');
+    const options = {
+        threshold: 0.3
+    }
+    allMenuItems.forEach(menuItem => {
+        const hashId = menuItem.hash;
+        const observedElement = document.querySelector(hashId);
+        let observer = new IntersectionObserver((entries) => {
+            const [entry] = entries;
+            if (entry.isIntersecting) {
+                menuItem.parentElement.classList.add("active");
+            } else {
+                menuItem.parentElement.classList.remove("active");
+            }
 
-let  allMenuItems = document.querySelectorAll('#flyout-menu-items li a, #static-menu-items li a');
-
-const options = {
-    threshold: 0.3
-}
-allMenuItems.forEach(menuItem => {
-    const hashId = menuItem.hash;
-    const observedElement = document.querySelector(hashId);
-    let observer = new IntersectionObserver((entries) => {
-        const [entry] = entries;
-        if (entry.isIntersecting) {
-            menuItem.parentElement.classList.add("active");
-        } else {
-            menuItem.parentElement.classList.remove("active");
-        }
-
-    }, options);
-    observer.observe(observedElement)
-})
-
+        }, options);
+        observer.observe(observedElement)
+    })
 })();
