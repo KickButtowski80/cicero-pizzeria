4cde48421846ad178f94d749b09888bd8bd1254a
kickbuttowski80 <pazpaz25@gmail.com>
2024-02-27 17:44:59 -0800

    set up intersection Observer Fixes #2

--- src/js/scrollMenFoodItemstoView.js
+++ src/js/scrollMenFoodItemstoView.js
@@ -1,1 +1,20 @@
-new IntersectionObserver();
+(() => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const intersecting = entry.isIntersecting;
+
+            if (intersecting) {
+                entry.target.style.backgroundColor = "blue";
+            }
+            else {
+                entry.target.style.backgroundColor = "green"
+            }
+        })
+    }
+        , { threshold: [0.4, 0.5] })
+
+    const foodMenu = document.getElementById('food-menu');
+    if (foodMenu) {
+        observer.observe(foodMenu);
+    }
+})();
