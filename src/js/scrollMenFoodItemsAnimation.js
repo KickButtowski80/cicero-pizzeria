e99248bbff52cebe441e49f579ce1364c6c568b6
kickbuttowski80 <pazpaz25@gmail.com>
2024-02-29 15:03:52 -0800

    change the name

---
+++ src/js/scrollMenFoodItemsAnimation.js
@@ -0,0 +1,27 @@
+(() => {
+    const images = document.querySelectorAll('.image')
+    const dinnerLogo = document.querySelector('.dinner-logo')
+    images.forEach(image => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                const intersecting = entry.isIntersecting;
+
+                if (intersecting) {
+                    image.classList.add('show-image');
+                    dinnerLogo.classList.add('dinner-logo')
+                }
+                else {
+                    image.classList.remove('show-image')
+                    dinnerLogo.classList.remove('dinner-logo')
+                }
+            })
+        }
+            , { threshold: 0.5 })
+        observer.observe(image);
+
+    })
+})();
+
+
+ 
+  
