b59dee604d7a084d0b81bf115bae1844f31c9ae4
kickbuttowski80 <pazpaz25@gmail.com>
2024-02-29 16:50:32 -0800

    add bouncing animation to lunch and dinner logos

--- src/js/scrollMenFoodItemsAnimation.js
+++ src/js/scrollMenFoodItemsAnimation.js
@@ -1,6 +1,7 @@
 (() => {
     const images = document.querySelectorAll('.image')
     const dinnerLogo = document.querySelector('.dinner-logo')
+    const lunchLogo = document.querySelector('.lunch-logo')
     images.forEach(image => {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
@@ -9,10 +10,12 @@
                 if (intersecting) {
                     image.classList.add('show-image');
                     dinnerLogo.classList.add('dinner-logo-animation')
+                    lunchLogo.classList.add('lunch-logo-animation')
                 }
                 else {
                     image.classList.remove('show-image')
                     dinnerLogo.classList.remove('dinner-logo-animation')
+                    lunchLogo.classList.remove('lunch-logo-animation')
                 }
             })
         }
@@ -23,5 +26,4 @@
 })();
 
 
- 
-  
+
