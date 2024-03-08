13e36329ea078dfa00cca737f908033e76448eaa
kickbuttowski80 <pazpaz25@gmail.com>
2024-03-08 15:36:36 -0800

    add work hours

---
+++ src/js/workHours.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let d = new Date();
+    let dayInNumber = d.getDay();
+    let workHours = document.getElementById("work-hours");
+    
+ 
+    let workSchedule = [
+      "Sunday 11AM–10PM",
+      "Monday 11AM–10PM",
+      "Tuesday 11AM–10PM",
+      "Wednesday 11AM–10PM",
+      "Thursday 11AM–10PM",
+      "Friday 11AM–11PM",
+      "Saturday 11AM–11PM"
+    ];
+    
+ 
+    let selectList = document.createElement("select");
+    selectList.id = "mySelect";
+    selectList.name = "mySelect";
+    selectList.className = 'text-black'
+    workHours.appendChild(selectList);
+    
+ 
+    for (var i = dayInNumber; i < workSchedule.length; i++) {
+        let option = document.createElement("option");
+        option.value = workSchedule[i];
+        option.text = workSchedule[i];
+        selectList.appendChild(option);
+    }
+  
+    for (var i = 0 ; i < dayInNumber; i++) {
+        let  option = document.createElement("option");
+        option.value = workSchedule[i];
+        option.text = workSchedule[i];
+        selectList.appendChild(option);
+    }
+  });
+  
