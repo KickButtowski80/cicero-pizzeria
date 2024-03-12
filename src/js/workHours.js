c55766670abc1e8996f78fcab5b6c7027334dc3c
kickbuttowski80 <pazpaz25@gmail.com>
2024-03-12 12:11:53 -0700

    clean up code and change type of variable to const

--- src/js/workHours.js
+++ src/js/workHours.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let d = new Date();
-    let dayInNumber = d.getDay();
-    let workHours = document.getElementById("work-hours");
-    
- 
-    let workSchedule = [
+    const  d = new Date();
+    const  dayInNumber = d.getDay();
+     
+    const  workSchedule = [
       "Sunday 11AM–10PM",
       "Monday 11AM–10PM",
       "Tuesday 11AM–10PM",
@@ -15,13 +13,9 @@ document.addEventListener("DOMContentLoaded", function() {
     ];
     
  
-    // let selectList = document.createElement("select");
-    // selectList.id = "mySelect";
-    // selectList.name = "mySelect";
-    // selectList.className = 'text-black';
-    let selectList = document.getElementById('hours-dropdown')
-    // selectList.style.width = "100%"
-    // workHours.appendChild(selectList);
+ 
+    const  selectList = document.getElementById('hours-dropdown')
+ 
     
  
     for (var i = dayInNumber; i < workSchedule.length; i++) {
