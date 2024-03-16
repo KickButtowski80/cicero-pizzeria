9c65a52e09a03407c1038c599914ee048caa90cf
kickbuttowski80 <pazpaz25@gmail.com>
2024-03-15 22:35:56 -0700

    get rid of reset form from frontend Fixes #50

--- src/js/resetContactForm.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById('contact-form'); // Get the form element
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent default form submission
-  form.reset();
-});
