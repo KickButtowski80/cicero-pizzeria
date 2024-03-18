555629e8d39ecc43a4def59b456ee3d068408e93
kickbuttowski80 <pazpaz25@gmail.com>
2024-03-18 14:29:51 -0700

    check if all the fields are available

--- api/contactFormEmailSender.js
+++ api/contactFormEmailSender.js
@@ -1,52 +1,52 @@
 
-const nodemailer = require("nodemailer"); 
+const nodemailer = require("nodemailer");
 require('dotenv').config()
 export default (request, response) => {
   if (request.method !== 'POST') {
-    return response.status(Status.BAD_REQUEST).send('');
+    return response.status(400).send('<h1>Bad Request </h1>');
   }
-  // const name = request?.body?.name ?? 'world';
-  // return response.json({
-  //   data: `hello ${name}`,
-  // });
 
-const {name, email, subject, message} =  request.body;
-console.log(name,email, subject, message)
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "pazpaz25@gmail.com",
-    pass: process.env.GMAIL_PASS,
-  },
-});
+  const { name, email, subject, message } = request.body;
+  if (!name || !email || !subject || !message) {
 
- 
-async function main() { 
-  const info = await transporter.sendMail({
-    from:  `${name} 🍕 <${email}>`, 
-    to: "pazpaz25@gmail.com", 
-    subject: `${subject} 😄`, 
-    text: `${message}`, 
-    html: `<b>${message}?</b>`, 
-  });
- 
-  console.log("Message sent: %s", info.messageId);
-  response.status(200).send(`
+    return response.status(400).send('<h1>Error</h1> <p>all the fields are required</p>');
+  }
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "pazpaz25@gmail.com",
+        pass: process.env.GMAIL_PASS,
+      },
+    });
+
+
+    async function main() {
+      const info = await transporter.sendMail({
+        from: `${name} 🍕 <${email}>`,
+        to: "pazpaz25@gmail.com",
+        subject: `${subject} 😄`,
+        text: `${message}`,
+        html: `<b>${message}?</b>`,
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      response.status(200).send(`
   <head>
   <base href="../index.html">
 </head>
   <h1>Go back</h1> <a href="#contact">go back</a>`)
- 
-  
-
- 
-}
- 
-main().catch(console.error);
-
-};
 
 
-  
+
+
+    }
+
+    main().catch(console.error);
+
+  };
+
+
+
