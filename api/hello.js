ddaabb9b4ac200f8eb3cb514a7bd915d409dccd4
kickbuttowski80 <pazpaz25@gmail.com>
2024-03-18 14:05:16 -0700

    try different route for base tag

--- api/hello.js
+++ api/hello.js
@@ -37,7 +37,7 @@ async function main() {
   console.log("Message sent: %s", info.messageId);
   response.status(200).send(`
   <head>
-  <base href="../src/index.html">
+  <base href="../index.html">
 </head>
   <h1>Go back</h1> <a href="src/sections/contact.html">go back</a>`)
  
