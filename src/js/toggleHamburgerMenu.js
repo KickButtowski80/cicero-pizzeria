778397b9d9ce940365e1e314e3e93685696485f3
kickbuttowski80 <pazpaz25@gmail.com>
2024-03-18 20:37:18 -0700

    set hamburger icon to 3 lines when menu items close Fixes #50

--- src/js/toggleHamburgerMenu.js
+++ src/js/toggleHamburgerMenu.js
@@ -1,24 +1,24 @@
 const hamburgerIcon = document.querySelector('#hamburgerIcon')
 const menuBg = document.querySelector('.menu-bg')
-const flyoutMenuId = document.querySelector('#flyout-menu-items');
+const flyoutMenuId = document.querySelectorAll('#flyout-menu-items');
 const flyoutMenuItems = document.querySelectorAll('#flyout-menu-items li');
 const navBarLogo = document.querySelector('#flyout-menu-logo');
-const navbar = document.getElementById('flyout-navbar')
-
+const flyoutNavBar = document.getElementById("flyout-navbar")
 hamburgerIcon.addEventListener('click', () => {
     hamburgerIcon.classList.toggle('change');
-    navbar.classList.toggle('show-flyout-navbar');
+    // flyoutNavBar.classList.toggle('show-flyout-navbar');
     toggleClasses(menuBg, 'in-view', 'out-view');
-    toggleClasses(flyoutMenuId, 'in-view-menuItems', 'out-view-menuItems');
+    toggleClasses(flyoutMenuId[0], 'in-view-menuItems', 'out-view-menuItems');
     toggleClasses(navBarLogo, 'in-view-logo', 'out-view-logo');
 
 });
-
-
-Array.from(flyoutMenuItems).slice(0, -1).forEach(fmi => fmi.addEventListener('click', () => {
-    flyoutMenuId.classList.remove('in-view-menuItems');
+ 
+ 
+Array.from(flyoutMenuItems).slice(0,-1).forEach(fmi => fmi.addEventListener('click', () => {
+    flyoutMenuId[0].classList.remove('in-view-menuItems');
     menuBg.classList.remove('in-view');
-    navBarLogo.classList.remove('in-view-logo');
+    navBarLogo.classList.remove('in-view-logo');   
+    hamburgerIcon.classList.remove('change')
 }))
 
 
@@ -31,4 +31,3 @@ function toggleClasses(element, class1, class2) {
         element.classList.add(class1);
     }
 }
-
