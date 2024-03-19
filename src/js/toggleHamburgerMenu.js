06e64809b34e9dd7b7fcdec43814c001f25f4bdc
kickbuttowski80 <pazpaz25@gmail.com>
2024-03-18 19:43:25 -0700

    fix showing menu items

--- src/js/toggleHamburgerMenu.js
+++ src/js/toggleHamburgerMenu.js
@@ -1,6 +1,6 @@
 const hamburgerIcon = document.querySelector('#hamburgerIcon')
 const menuBg = document.querySelector('.menu-bg')
-const flyoutMenuId = document.querySelectorAll('#flyout-menu-items');
+const flyoutMenuId = document.querySelector('#flyout-menu-items');
 const flyoutMenuItems = document.querySelectorAll('#flyout-menu-items li');
 const navBarLogo = document.querySelector('#flyout-menu-logo');
 const navbar = document.getElementById('flyout-navbar')
@@ -9,14 +9,14 @@ hamburgerIcon.addEventListener('click', () => {
     hamburgerIcon.classList.toggle('change');
     navbar.classList.toggle('show-flyout-navbar');
     toggleClasses(menuBg, 'in-view', 'out-view');
-    toggleClasses(flyoutMenuId[0], 'in-view-menuItems', 'out-view-menuItems');
+    toggleClasses(flyoutMenuId, 'in-view-menuItems', 'out-view-menuItems');
     toggleClasses(navBarLogo, 'in-view-logo', 'out-view-logo');
 
 });
 
 
 Array.from(flyoutMenuItems).slice(0, -1).forEach(fmi => fmi.addEventListener('click', () => {
-    flyoutMenuId[0].classList.remove('in-view-menuItems');
+    flyoutMenuId.classList.remove('in-view-menuItems');
     menuBg.classList.remove('in-view');
     navBarLogo.classList.remove('in-view-logo');
 }))
