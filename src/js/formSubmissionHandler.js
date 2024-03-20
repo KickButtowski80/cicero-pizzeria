b26f5369dadbf818674563c89e1d88471db7c27b
kickbuttowski80 <pazpaz25@gmail.com>
2024-03-19 20:41:34 -0700

    convert to json format

--- src/js/formSubmissionHandler.js
+++ src/js/formSubmissionHandler.js
@@ -3,19 +3,19 @@ const form = document.getElementById('contact-form');
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var subject = document.getElementById('subject').value;
-    var message = document.getElementById('message').value;
-    const dataForm = {
-        name, email, subject, message
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+    const formData = {
+        name:name, email:email, subject:subject, message:message
     }
     // const formData = new FormData(dataForm);
 
 
     const response = await fetch('/api/contactFormEmailSender.js', {
         method: 'POST',
-        body: dataForm
+        body: JSON.stringify(formData)
     })
     const data = response.json()
 debugger;
