996569a622d71271c87c9740bba6fe63f298fcc4
kickbuttowski80 <pazpaz25@gmail.com>
2024-04-01 13:50:07 -0700

    format

--- src/js/formSubmissionHandler.js
+++ src/js/formSubmissionHandler.js
@@ -8,30 +8,30 @@ form.addEventListener('submit', async function (e) {
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
     const formData = {
-        name:name, email:email, subject:subject, message:message
+        name: name, email: email, subject: subject, message: message
     }
     // const formData = new FormData(dataForm);
 
     try {
-    const response = await fetch('/api/contactFormEmailSender.js', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData)
-    })
-    const data = await response.json()
+        const response = await fetch('/api/contactFormEmailSender.js', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        })
+        const data = await response.json()
 
 
-    if (data.success) {
-        alert(`${data.message}`);
-    } else {
-        alert('There was an error adding the fields.', data.message);
+        if (data.success) {
+            alert(`${data.message}`);
+        } else {
+            alert('There was an error adding the fields.', data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
     }
-} catch (error) {
-    console.error('Error:', error);
-  }
 
 
 })
