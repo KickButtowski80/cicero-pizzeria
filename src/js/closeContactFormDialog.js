0479fa38d94f6c92d8431aca1fbb4894ae3155bc
kickbuttowski80 <pazpaz25@gmail.com>
2024-04-01 18:29:14 -0700

    choose better naming

--- src/js/closeContactFormDialog.js
+++ src/js/closeContactFormDialog.js
@@ -1,9 +1,9 @@
 
 (() => {
-    const contactFormCloseBtn = document.querySelector("#response-form-dialog  button");
+    const contactFormDialogCloseBtn = document.querySelector("#response-form-dialog  button");
     const contactFormDialog = document.querySelector("#response-form-dialog");
     const contactForm = document.querySelector("#contact-form");
-    contactFormCloseBtn.addEventListener('click', () => {
+    contactFormDialogCloseBtn.addEventListener('click', () => {
         contactForm.reset();
         contactFormDialog.style.display = "none";
     });
