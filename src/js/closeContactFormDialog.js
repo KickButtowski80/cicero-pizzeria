f4ad26c5aab985c470e7fcc83df4b7bf4ddc2de1
kickbuttowski80 <pazpaz25@gmail.com>
2024-04-01 18:19:56 -0700

    clear form after the continue shopping btn is clicked

--- src/js/closeContactFormDialog.js
+++ src/js/closeContactFormDialog.js
@@ -4,5 +4,6 @@
     const contactForm = document.querySelector("#response-form-dialog");
     contactFormCloseBtn.addEventListener('click', () => {
         contactForm.style.display = "none";
+        contactForm.reset();
     });
 })();
