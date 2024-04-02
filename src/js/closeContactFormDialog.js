72882019be316e3ee55e002d7b96339d24387356
kickbuttowski80 <pazpaz25@gmail.com>
2024-04-01 18:08:56 -0700

    close dialog when the close btn is clicked

---
+++ src/js/closeContactFormDialog.js
@@ -0,0 +1,6 @@
+const contactFormCloseBtn = document.querySelector("#response-form-dialog > button");
+contactFormCloseBtn.addEventListener('click', ()=>{
+   contactFormCloseBtn.style.display = "none"
+ 
+ 
+});
