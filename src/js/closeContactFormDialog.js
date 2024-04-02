1da2a80d4f08163fa39be01cc3f2a85ac426269e
kickbuttowski80 <pazpaz25@gmail.com>
2024-04-01 18:14:24 -0700

    covert iffe

--- src/js/closeContactFormDialog.js
+++ src/js/closeContactFormDialog.js
@@ -1,6 +1,9 @@
+
+(() => {
 const contactFormCloseBtn = document.querySelector("#response-form-dialog  button");
 contactFormCloseBtn.addEventListener('click', ()=>{
    contactFormCloseBtn.style.display = "none"
    console.log('i am here')
  
 });
+})();
