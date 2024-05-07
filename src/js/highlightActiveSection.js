ab381002d40c43b9a14ee1d56e7077038abc3493
kickbuttowski80 <pazpaz25@gmail.com>
2024-05-06 18:58:03 -0700

    fix pizza indicator for menu item issue

--- src/js/highlightActiveSection.js
+++ src/js/highlightActiveSection.js
@@ -1,16 +1,17 @@
 (() => {
     let allMenuItems = document.querySelectorAll('#flyout-menu-items li a, #static-menu-items li a');
-
+    const currentPathName = window.location.pathname;
     const options = {
-        threshold: window.location.pathname.includes('/lunch.html' || '/dinner.html') ? 0 : 0.3
+        threshold: currentPathName.includes('/lunch.html') ||
+            currentPathName.includes('/dinner.html') ? 0 : 0.3
     }
 
     allMenuItems.forEach(menuItem => {
-
         const hashId = menuItem.hash;
         const observedElement = document.querySelector(hashId);
         if (observedElement === null) return;
         let observer = new IntersectionObserver((entries) => {
+            // debugger;
             const [entry] = entries;
             if (entry.isIntersecting) {
                 menuItem.parentElement.classList.add("active");
