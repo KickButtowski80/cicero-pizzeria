8c9ac5e8bfd7102195f847a8a39fabd37be029e4
kickbuttowski80 <pazpaz25@gmail.com>
2024-05-07 17:53:22 -0700

    refactor not found item in the menu logic

--- src/js/readWriteDinnerLunchJson.js
+++ src/js/readWriteDinnerLunchJson.js
@@ -42,21 +42,10 @@ function loadMenu(data, mealType, searchWord) {
         searchedMenus[category] = menuItems;
       }
     }
+ 
     if (Object.keys(searchedMenus).length === 0) {
-      menuList.innerHTML = `
-      <div id="alert-additional-content-1" class="mb-4 rounded-lg border border-gray-300 bg-blue-50 p-4 text-gray-800 dark:border-blue-800 dark:bg-gray-800 dark:text-gray-400" role="alert">
-      <div class="flex items-center">
-        <svg class="me-2 h-4 w-4 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-        </svg>
-        <span class="sr-only">Info</span>
-        <h3 class="text-lg font-medium">there is no ${searchWord} item in the ${mealType} menu.</h3>
-      </div>
-      <p>please try different name</p>
-    </div>`
-      return;
+      renderNotFoundItem( mealType, searchWord)
     }
-
     renderMenuItems(searchedMenus, menuList, searchWord)
   }
 
@@ -87,7 +76,23 @@ function loadMenu(data, mealType, searchWord) {
       categoryCard.appendChild(itemsCard);
     }
   }
+ function renderNotFoundItem( mealType, searchWord){
 
+
+      menuList.innerHTML = `
+      <div id="alert-additional-content-1" class="mb-4 rounded-lg border border-gray-300 bg-blue-50 p-4 text-gray-800 dark:border-blue-800 dark:bg-gray-800 dark:text-gray-400" role="alert">
+      <div class="flex items-center">
+        <svg class="me-2 h-4 w-4 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <span class="sr-only">Info</span>
+        <h3 class="text-lg font-medium">there is no ${searchWord} item in the ${mealType} menu.</h3>
+      </div>
+      <p>please try different name</p>
+    </div>`
+      return;
+    
+  }
 
   // Function to create category card
   function createCategory(category, description) {
