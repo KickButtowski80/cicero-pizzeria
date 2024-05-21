3bfc42114405a6df7c4409121c617829571495d4
kickbuttowski80 <pazpaz25@gmail.com>
2024-05-20 17:06:06 -0700

    rid of the debugging statement Fixes #63

--- src/js/highlightSearchWord.js
+++ src/js/highlightSearchWord.js
@@ -1,7 +1,8 @@
 const highlightCategory = (category, searchWord ) => {
-    if (searchWord === undefined) {
-        return category;
-    }
+    // if (searchWord === undefined) {
+    //     return category;
+    // }
+    debugger;
   const regex = new RegExp(searchWord, "gi");
     category = category.replace(regex, `<mark>$&</mark>`);
     return category;
